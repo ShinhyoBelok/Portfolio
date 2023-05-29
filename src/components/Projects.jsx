7185e1e1d3309d6c0c01f9f077a10fe12feff776
ShinhyoBelok <shibo6937@gmail.com>
2023-05-29 18:29:04 -0400

    Add project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <section class="project" id="project-${i}">
+      {/* <img class="snap ${projects[i].contentDirection}" src="${projects[i].image}" alt="First project card picture" />
+      <h2>${projects[i].name}</h2>
+      <section>
+          <ul class="canopy">
+              <li>CANOPY</li>
+              <li><img src="${projects[i].dot}" alt="Dot icon"/></li>
+              <li><span class="lightgray">${projects[i].canopyli1}</span></li>
+              <li><img src="${projects[i].dot}" alt="Dot icon" /></li>
+              <li><span class="lightgray">${projects[i].canopyli2}</span></li>
+          </ul>
+      </section>
+      <p>${projects[i].resume}</p>
+      <ul class="programmingLanguages">
+        ${createProgrammingLanguages(i)}
+      </ul> */}
+      <button class="button detail" id="${projects[i].btnId}">See Project</button>
+  </section>
   )
 }
