ee80aeb878d8e2f96d8c8a94534c8db91a63e959
ShinhyoBelok <shibo6937@gmail.com>
2023-05-29 18:28:28 -0400

    Add navbar html

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav class="menu">
+      <ul>
+          <li class="nav-item">
+              <a href="#work"><p>Portfolio</p></a>
+          </li>
+          <li class="nav-item">
+              <a href="#aboutContainer"><p>About</p></a>
+          </li>
+          <li class="nav-item">
+              <a href="#contactForm"><p>Contact</p></a>
+          </li>
+      </ul>
+      <img class="bar open-menu" src="img\Icon - Menu.svg" alt="hambuger icon" />
+      <img class="bar close-menu" src="img\Icon - Cancel.svg" alt="hambuger icon" />
+  </nav>
   )
 }
