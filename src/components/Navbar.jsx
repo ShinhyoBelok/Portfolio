e747427a5fa6b751240c193a2b1e2d337d5e63af
ShinhyoBelok <shibo6937@gmail.com>
2023-06-01 21:28:06 -0400

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
+import './stylesheets/navbar.css'
 
 export default function Navbar() {
   return (
-    <nav class="menu">
-      <ul>
-          <li class="nav-item">
-              <a href="#work"><p>Portfolio</p></a>
-          </li>
-          <li class="nav-item">
-              <a href="#aboutContainer"><p>About</p></a>
-          </li>
-          <li class="nav-item">
-              <a href="#contactForm"><p>Contact</p></a>
-          </li>
-      </ul>
-      <img class="bar open-menu" src="img\Icon - Menu.svg" alt="hambuger icon" />
-      <img class="bar close-menu" src="img\Icon - Cancel.svg" alt="hambuger icon" />
-  </nav>
+    <nav className="menu d-flex">
+        <h3>Shinhyo</h3>
+        <ul className='d-flex'>
+            <li class="nav-item">
+                <a href="#work"><p>Portfolio</p></a>
+            </li>
+            <li class="nav-item">
+                <a href="#aboutContainer"><p>About</p></a>
+            </li>
+            <li class="nav-item">
+                <a href="#contactForm"><p>Contact</p></a>
+            </li>
+        </ul>
+      {/* <img class="bar open-menu" src="img\Icon - Menu.svg" alt="hambuger icon" />
+      <img class="bar close-menu" src="img\Icon - Cancel.svg" alt="hambuger icon" /> */}
+    </nav>
   )
 }
